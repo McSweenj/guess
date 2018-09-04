4bb1b6894c7a5927cf91f4354a1ff52db1425909
McSweenj <42870470+McSweenj@users.noreply.github.com>
2018-09-04 11:31:29 -0400

    Update commentes.js

--- commentes.js
+++ commentes.js
@@ -6,19 +6,30 @@ var Turns=0;
 var Correct=false;
 // adds 1 to the varible turns 
 Turns++;
-// 
+// keep looping till it is set to true 
 while (Correct==false) {
+    // anishlizing guess set val to user imput tellinguser to imput 
     var Guess = prompt("Guess my integer (between 0 and 100)!");
+    // cheking to see if guess it = to anser 
     if (Guess == Answer) {
+        // corret and num turnes 
         alert("CORRECT! You guessed it in "+Turns+" turns.");
+        // set eqal to true to get out of "wile loop "
         Correct = true;
     }
+    // chek to see if its the same as anser  
     else if (Guess > Answer) {
+        // the guess it the same as the anser
         alert("Hint: Try a lower number!");
+        // tell you to guess lower num
     }
-    else if (Guess < Answer) {
+    else if (Guess < Answer)
+        // not eqal to the anser 
         alert("Hint: Try a higher number!");
+    // tell you to guess higher num
     }
     Turns++;
+// tells you the num of turns you took 
 }
 alert("Thank you for playing.");
+// thanks you for playing after you guess the right anser 
